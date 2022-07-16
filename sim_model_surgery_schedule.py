b2ba1ae4d8a3215d21a10801927fb2908dda92e9
Ramachandran Janardhan <rjanardhan@gmail.com>
2022-07-15 22:33:14 -0400

    complete schedule model and streamlit starting viz

--- sim_model_surgery_schedule.py
+++ sim_model_surgery_schedule.py
@@ -2,12 +2,22 @@ import numpy as np
 import simpy
 import random
 import copy
+import streamlit as st
+import pandas as pd
 
 np.random.seed(0)
 
+entitlement_select = st.slider("Select Knees Entitlement", 1, 15, 1)
+
 SURG_TIME = 0.0
-CONS_INV = [{'surg_type': 'primary knee', 'entitlement': 8, 'onHand': 8, 'openOrder': 0},
-            {'surg_type': 'primary hip', 'entitlement': 5, 'onHand': 6, 'openOrder': 0}]
+CONS_INV = [{'surg_type': 'primary knee', 'entitlement': 0, 'onHand': 0, 'openOrder': 0, 'orderQty': 0},
+            {'surg_type': 'primary hip', 'entitlement': 5, 'onHand': 6, 'openOrder': 0, 'orderQty': 0}]
+
+CONS_INV[0]['entitlement'] = entitlement_select + 1
+CONS_INV[0]['onHand'] = entitlement_select + 1
+
+O_R = [{'surg_type': 'primary knee', 'orderDay': [], 'orderQty': [], 'receiptDay': [], 'receiptQty': []},
+       {'surg_type': 'primary hip', 'orderDay': [], 'orderQty': [], 'receiptDay': [], 'receiptQty': []}]
 
 WAREHOUSE = [{'surg_type': 'primary knee', 'entitlement': 30, 'onHand': 300, 'openOrder': 0},
              {'surg_type': 'primary hip', 'entitlement': 30, 'onHand': 300, 'openOrder': 0}]
@@ -18,6 +28,7 @@ LT_PROB = [0.45, 0.3, 0.2, 0.05]
 
 TIME_HIST = []
 INV_HIST = []
+LT_HIST = []
 temp = []
 
 
@@ -64,12 +75,12 @@ class Surgery:
         self.env = env
         self.schedule = copy.deepcopy(SCHED)
         self.cons = copy.deepcopy(CONS_INV)
+        self.order_receipts = copy.deepcopy(O_R)
         self.warehouse = copy.deepcopy(WAREHOUSE)
         self.surgery = env.event()
         env.process(self.sEvent(self.env, self.schedule))
 
     def sEvent(self, env, schedule):
-
         for index, item in enumerate(schedule):
             if index == 0:
                 yield env.timeout(self.schedule[0]['surg_day'])
@@ -94,15 +105,22 @@ class Surgery:
             self.surgery = self.env.event()
             env.process(self.replenishOrder(self.env, c_index, index))
             TIME_HIST.append(env.now)
-            INV_HIST.append(copy.deepcopy(self.cons))
+            INV_HIST.append(copy.deepcopy(self.cons[c_index]))
 
     def replenishOrder(self, env, c, i):
-
         if self.cons[c]['onHand'] < self.cons[c]['entitlement']:
             orderQuantity = max(
                 self.cons[c]['entitlement'] - self.cons[c]['onHand'] - self.cons[c]['openOrder'], 0)
+            self.cons[c]['orderQty'] = orderQuantity
             self.cons[c]['openOrder'] += orderQuantity
-            yield env.timeout(self.getLT())
+            self.order_receipts[c]['orderDay'].append(env.now)
+            self.order_receipts[c]['orderQty'].append(orderQuantity)
+
+            lead_time = self.getLT()
+            yield env.timeout(lead_time)
+            LT_HIST.append(lead_time)
+            self.order_receipts[c]['receiptDay'].append(env.now)
+            self.order_receipts[c]['receiptQty'].append(orderQuantity)
             self.cons[c]['onHand'] += orderQuantity
             self.cons[c]['openOrder'] -= orderQuantity
 
@@ -114,6 +132,27 @@ class Surgery:
 env = simpy.Environment()
 s = Surgery(env, SCHED, CONS_INV)
 env.run()
-
 for index, item in enumerate(INV_HIST):
-    print(TIME_HIST[index], item)
+    item['time'] = TIME_HIST[index]
+
+df_orderlog = pd.DataFrame()
+df = pd.DataFrame(INV_HIST)
+df_sched = pd.DataFrame(s.schedule)
+
+for i in range(len(s.order_receipts)):
+    mydict = s.order_receipts[i]
+    df_temp = pd.DataFrame(dict(list(mydict.items())[1:]))
+    df_temp['surg_type'] = mydict['surg_type']
+    df_orderlog = pd.concat([df_orderlog, df_temp])
+
+# Streamlit Portion
+
+
+option = st.selectbox(
+    'Select Surgery Type',
+    ('primary knee', 'primary hip'))
+
+x = (df[df.surg_type == option]['onHand'])
+
+st.write("""#My App - Jamadu Ka Gulla""")
+st.line_chart(x)
